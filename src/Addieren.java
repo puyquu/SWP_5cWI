78da7a86f8a6a7009110b69bfcbe787c3573dde1
zech.emanuel <emanuel.zech@gmail.com>
2022-10-13 15:17:16 +0200

    addieren finished

--- src/Addieren.java
+++ src/Addieren.java
@@ -14,44 +14,38 @@ public class Addieren {
         Scanner number1 = new Scanner(System.in);
         Scanner number2 = new Scanner(System.in);
 
-        int temp1 = number1.nextInt();
-        int temp2 = number2.nextInt();
+        int repetitions;
+
+        int firstNumber = number1.nextInt();
+        int secondNumber = number2.nextInt();
+
+        int length1 = String.valueOf(firstNumber).length();
+        int length2 = String.valueOf(secondNumber).length();
+
+        if (length1 >= length2) {
+            repetitions = length1 + 1;
+        } else {
+            repetitions = length2 + 1;
+        }
 
 
-        int length1 = String.valueOf(temp1).length();
-        int length2 = String.valueOf(temp2).length();
-
-
-        int repetitions = (length1 + length2) / 2 + 2;
-
-        int firstNumber = temp1;
-        int secondNumber = temp2;
         for (int i = 0; i < repetitions; i++) {
-
-                int partialresult = firstNumber % 10 + secondNumber % 10;
-                int addup = partialresult / 10;
-            if(firstNumber != 0 && secondNumber != 0 | addup != 0){
-                int rest = partialresult % 10;
-
-
+            int partialresult = firstNumber % 10 + secondNumber % 10;
+            int addup = partialresult / 10;
+            if (partialresult == 0 && firstNumber == 0 && secondNumber == 0) {
+                break;
+            } else {
+                System.out.println(partialresult % 10);
                 if (partialresult >= 10) {
-                    System.out.println(rest);
-
-
-                    firstNumber = firstNumber/10 + addup;
-                    secondNumber = secondNumber/10;
-
-
+                    firstNumber = firstNumber / 10 + addup;
+                    secondNumber = secondNumber / 10;
                 } else {
-                    System.out.println(partialresult);
-                    firstNumber = firstNumber/10;
-                    secondNumber = secondNumber/10;
-
+                    firstNumber = firstNumber / 10;
+                    secondNumber = secondNumber / 10;
                 }
-
-
             }
-            }
-
+        }
     }
 }
+
+
